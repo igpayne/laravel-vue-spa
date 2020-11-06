8168aec2b2e3431ffec4a8f7840e552b3544abc1
Isaac Payne <isaac.payne@hotmail.com>
2020-11-06 15:16:03 +0000

    Set up Vue and loaded simple Bootstrap template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+//require vue
+window.Vue = require("vue");
+
+//attach the 'front-page' component to 'frontView' template
+Vue.component("front-page", require("./components/frontView.vue").default);
+
+//create a new vue instance with id: app
+const app = new Vue({
+    el: "#app",
+});
