9a214592224c1ea902521023dbaddd15f0990a85
Isaac Payne <isaac.payne@hotmail.com>
2020-11-07 23:16:43 +0000

    Modified Release factory

--- database/factories/ReleaseFactory.php
+++ database/factories/ReleaseFactory.php
@@ -24,7 +24,7 @@ class ReleaseFactory extends Factory
         return [
             "name" => $this->faker->word . " " . $this->faker->word,
             "artist_name" => $this->faker->word,
-            "description" => $this->faker->word,
+            "description" => $this->faker->sentence,
             "release_date" => $this->faker->dateTimeBetween($startDate = "0 years", $endDate = "+10 years")
         ];
     }
