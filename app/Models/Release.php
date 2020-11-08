338b01c8b3dd8230c85f596fc4f708621caaa917
Isaac Payne <isaac.payne@hotmail.com>
2020-11-08 02:26:34 +0000

    Added eloquent relationships to models

--- app/Models/Release.php
+++ app/Models/Release.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Release extends Model
 {
     use HasFactory;
+
+    public function tracks() {
+        return $this->hasMany("App\Models\Track");
+    }
 }
--- app/Models/Track.php
+++ app/Models/Track.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Track extends Model
 {
     use HasFactory;
+
+    public function release() {
+        return $this->belongsTo("App\Models\Release");
+    }
 }
