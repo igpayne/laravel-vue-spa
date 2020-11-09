76310f0c480d6e7f699e5a8aff6c9c9d5d7a5efe
Isaac Payne <isaac.payne@hotmail.com>
2020-11-09 00:29:58 +0000

    Clicking a genre will now filter the releases

--- resources/js/components/ReleasesPanel.vue
+++ resources/js/components/ReleasesPanel.vue
@@ -1,19 +1,25 @@
 <template>
+<div>
+    
+    <div class="text-center">
+        <h1 class="mb-5 mt-5 text-left">Releases</h1>
+    </div>
+    
     <div class="row">
-
-          <div class="col-lg-4 col-md-6 mb-4" v-for="release in releases" :key="release.id">
+        <div class="col-lg-4 col-md-6 mb-4" v-for="release in releases" :key="release.id">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#">{{release.name}}</a>
+                    <a href="#">{{release.name}}</a>
                 </h4>
                 <p class="card-text">{{release.description}}</p>
-              </div>
+                </div>
             </div>
-          </div>
-
         </div>
+    </div>
+
+</div>
 </template>
 
 <script>
